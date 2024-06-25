208b8430b5736fae90b21e2dc904f873ac7461d9
Felipe Almeida <felipealmeida37@yahoo.com.br>
2024-06-25 19:53:47 -0300

    Adicionado exercicios

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois numeros inteiros e imprima a divisao inteira do primeiro pelo segundo
+
+numero_01 = int(input("Digite o primeiro numero: "))
+numero_02 = int(input("Digite o segundo numero: "))
+
+print(numero_01//numero_02)
