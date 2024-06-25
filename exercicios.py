d5a4882c5bb78198bb5617e3c93cafa616a394a0
Felipe Almeida <felipealmeida37@yahoo.com.br>
2024-06-25 20:12:40 -0300

    Adicionado exercicio de formatação de data

--- exercicios.py
+++ exercicios.py
@@ -12,4 +12,14 @@ print(numero_01//numero_02)
 
 raio_do_circulo = float(input("Digite um raio: "))
 area_do_circulo = math.pi * raio_do_circulo ** 2
-print(f"{area_do_circulo:.2f}")
+print(f"{area_do_circulo:.2f}") #faz com que formate o resultado para somente 2 casa decimais
+
+
+# Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mes e o ano separadamente
+
+data_informada = input("Insira uma data no formato dd/mm/aaaa: ")
+lista_de_dia_mes_ano = data_informada.split("/")
+
+print(f"O elemento 1 é o : {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 2 é o : {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 3 é o : {lista_de_dia_mes_ano[2]}")
